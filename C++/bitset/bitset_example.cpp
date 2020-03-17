a89a018b26bd1f624b9fca066a5a8eaa7845832a
ychenchen <whutycc@163.com>
2020-03-17 10:44:13 +0800

    bool_vector_and_bitset

---
+++ C++/bitset/bitset_example.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+template <size_t n>
+inline void CL(bitset<n>& S, size_t w)
+{
+	w %= n;
+	S = S << w | S >> (n - w);
+}
+
+template <size_t n>
+inline void CR(bitset<n>& S, size_t w)
+{
+	w %= n;
+	S = S >> w | S << (n - w);
+}
+
+int main()
+{
+	const size_t n = 16;
+	bitset<n> A;
+	cout << A.size() << endl;
+	size_t pos = 7;
+	A.set(pos);
+	cout << A.count() << endl;
+	A.set(pos, false);
+	cout << A.count() << endl;
+	cout << (A[pos] == true) << endl;
+	A.flip(pos);
+	cout << A.count() << endl;
+	A.flip();
+	cout << A.count() << endl;
+	cout << A << endl;
+	for (size_t i = 0; i < A.size(); ++i)
+		cout << A[i];
+	cout << endl;
+ 	A >> 1;
+	cout << A << endl;
+
+	size_t number = 5;
+	bitset<n> B(number);
+	cout << B << endl;
+	size_t w = 4;
+	auto C = B << 4;
+	cout << B << endl;
+	cout << C << endl;
+	B >>= w;
+	cout << B << endl;
+	C >>= w;
+	cout << C << endl;
+	CL(C, w);
+	cout << C << endl;
+	CR(C, w);
+	cout << C << endl;
+
+	return 0;
+}
---
+++ C++/bitset/bool_vector_and_bitset.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	const size_t n = 60000000;
+	vector<bool> A(n, true);
+	bitset<n> B;
+	B.flip();
+	double start_t, end_t;
+	const size_t loop = 100;
+	start_t = clock();
+	for (size_t i = 0; i < loop; ++i)
+		for (size_t j = 0; j < A.size(); ++j)
+			if (j % 2 == 1)
+				A[j] = false;
+	end_t = clock();
+	std::cout << (end_t - start_t) / (CLOCKS_PER_SEC * 60)
+		<< " minutes" << std::endl;
+
+	start_t = clock();
+	for (size_t j = 0; j < B.size(); ++j)
+		if (j % 2 == 1)
+			B[j] = false;
+	end_t = clock();
+	std::cout << (end_t - start_t) / (CLOCKS_PER_SEC * 60)
+		<< " minutes" << std::endl;
+	return 0;
+}
