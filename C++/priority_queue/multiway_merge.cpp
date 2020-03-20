4d9066c1501f6a4a536f0d97c2c9a096147cbaf9
ychenchen <whutycc@163.com>
2020-03-20 08:50:42 +0800

    multiway merge

---
+++ C++/priority_queue/multiway_merge.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+template <typename T>
+void merge(const vector<vector<int>>& V, vector<T>& S)
+{
+	// 统计有序序列的元素总数n.
+	size_t n = 0;
+	for (const auto& X : V)
+		n += X.size();
+	S.clear();
+	S.reserve(n);
+	// 以范围形式描述当前待处理的有序序列.
+	using range = pair<decltype(V[0].begin()), decltype(V[0].end())>;
+	auto cmp = [](range a, range b) { return *(b.first) < *(a.first); };
+	priority_queue<range, vector<range>, decltype(cmp)> PQ(cmp);
+	for (const auto & X : V)
+		if (X.begin() != X.end())
+			PQ.push({X.begin(), X.end()});
+	while (!PQ.empty())
+	{
+		auto interval = PQ.top();
+		PQ.pop();
+		S.push_back(*(interval.first++));
+		if (interval.first != interval.second)
+			PQ.push(interval);
+	}
+	
+}
+
+
+int main()
+{
+	vector<vector<int>> A = {{1, 2, 4}, {}, {2, 3, 5}, {3, 4, 6, 8}};
+	// A[0], A[1], A[2], A[3]均为有序向量.
+	vector<int> B;
+	merge(A, B);
+	for (const auto& x : B)
+		cout << x << endl;
+	return 0;
+}
