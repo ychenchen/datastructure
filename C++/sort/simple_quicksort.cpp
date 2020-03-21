577d5883feb30ddec8022c89387c46d2c4f9e7f0
ychenchen <whutycc@163.com>
2020-03-21 16:50:02 +0800

    quicksort

---
+++ C++/sort/simple_quicksort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 最好使用C++17编译本程序.
+
+template <typename iterator>
+void quicksort(iterator left, iterator right)
+{
+	if (left +1 < right)
+	{
+		auto z = *left;
+		iterator pivot = partition(left + 1, right, [z](const auto& x) { return x < z; }) - 1;
+		swap(*left, *pivot);
+		quicksort(left, pivot);
+		quicksort(pivot + 1, right);
+	}
+}
+
+int main()
+{
+	vector<int> V = {2, 1, 3, 4, 5};
+	quicksort(V.begin(), V.end());
+	for (const auto& x : V)
+		cout << x << ' ';
+	cout << endl;
+	return 0;
+}
