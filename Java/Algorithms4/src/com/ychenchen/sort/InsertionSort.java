4c603bff16a773f417c0b8af3166f6cce483e4fc
ychenchen <whutycc@163.com>
2020-03-31 09:49:57 +0800

    insertion sort

--- Java/Algorithms4/src/com/ychenchen/sort/InsertionSort.java
+++ Java/Algorithms4/src/com/ychenchen/sort/InsertionSort.java
@@ -11,8 +11,14 @@ public class InsertionSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 1; i < N; i++) {   //i从1开始，每次循环完成第i个元素的向前插入排序
-            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
-                exch(a, j, j - 1);
+//            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+//                exch(a, j, j - 1);
+//            }
+            for (int j = i; j > 0; j--) {
+                if (less(a[j], a[j - 1]))
+                    exch(a, j, j - 1);
+                else
+                    break;
             }
         }
     }
